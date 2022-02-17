c2e1e50414b844feffaf5bb65e95fdd38d95cf74
Tommybarral <isamutom27@hotmail.com>
2022-02-17 15:37:10 +0100

    CategoryItem and theming added

--- lib/Screens/category_screen.dart
+++ lib/Screens/category_screen.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import '../Models/DUMMY_DATA.dart';
+import '../Widgets/category_item.dart';
 
 class CategoryScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals App'),
+        centerTitle: true,
       ),
-      children: [
-        // use data there.
-      ],
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((catData) {
+            return Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              child: CategoryItem(catData.title, catData.color),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mealsapp/Screens/category_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,27 +12,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        //colorScheme: Theme.of(context).colorScheme.copyWith(primary
+          //  : Colors.deepOrange),
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1),),
+          bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1),),
+          subtitle1: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Meals Application'),),
-      body: Center(
-        child: Text('Welcome to Meals App'),
-      ),
+      home: CategoryScreen(),
     );
   }
 }
