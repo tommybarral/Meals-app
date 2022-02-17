1e30c523c43e1d60b7f2f7ec98d6f0f9b1aa7190
Tommybarral <isamutom27@hotmail.com>
2022-02-17 13:53:42 +0100

    model & dummy_data added

---
+++ lib/Screens/category_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      children: [
+        // use data there.
+      ],
+    );
+  }
+}
