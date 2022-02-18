2887f7b363b1819638a5d21c6ff80602f33e99d2
Tommybarral <isamutom27@hotmail.com>
2022-02-18 13:15:19 +0100

    Category meals & navigator added

--- lib/Widgets/category_item.dart
+++ lib/Widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp/Screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
 
@@ -7,18 +8,29 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    },),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-        borderRadius: BorderRadius.circular(15),
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(title, style: Theme.of(context).textTheme.subtitle1,),
       ),
-      child: Text(title, style: Theme.of(context).textTheme.subtitle1,),
     );
   }
 }
